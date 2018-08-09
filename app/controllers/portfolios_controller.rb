369ac72d5821d4f41d4c4f600259639f370c232d
bergie88 <frazberg@yahoo.com>
2018-08-09 12:14:08 -0400

    Added Destroy Feature on Portfolios-Feature Branch

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,4 +40,16 @@ def update
     end
   end
 
+  def destroy
+    #Performs the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    #Destroys the record
+    @portfolio_item.destroy
+    #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio Item was successfully destroyed.' }
+    
+    end
+  end
+
 end
